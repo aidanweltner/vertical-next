5104a3ee8c030bd3331f77b65020ff278c22bc00
aidanweltner <aidan.weltner@gmail.com>
2020-12-28 16:35:34 -0700

    added mobile dropdown hamburger menu

--- components/mobileMenu.js
+++ components/mobileMenu.js
@@ -1,31 +1,44 @@
-import { Menu } from "@headlessui/react";
+import { Transition } from "@headlessui/react";
 import Link from "next/link";
-import Burger from "./burger/burger";
 import LogoHoriz from "./logoHoriz"
+import { useState } from "react"
+import MenuItems from "./menuItems";
 
 export default function MobileMenu({dark}) {
+  const [ isOpen, toggle ] = useState(false)
+
   return (
     <div className="block lg:hidden">
-      <Menu>
-        <div className="flex items-center" >
-          <Menu.Button>
-            <Burger /* isOpen={isOpen} *//>
-            <span className="sr-only">Menu</span>
-          </Menu.Button>
-          <Link href="/">
-            <a className="border-none">
-              <LogoHoriz className={`h-10 lg:hidden ml-4 ${dark ? 'text-white' : 'text-primary'}`}/>
-            </a>
-          </Link>
-        </div>
-        <Menu.Items as="nav" className="my-4" >
-          <Menu.Item>
-            <Link href="/about">
-              <a>About Us</a>
-            </Link>
-          </Menu.Item>
-        </Menu.Items>
-      </Menu>
+      <div className="flex items-center" >
+        <button onClick={() => toggle(!isOpen)}>
+          <div className={ isOpen ? `burger__open` : `burger` } >
+            <span/>
+            <span/>
+            <span/>
+          </div>
+          <span className="sr-only">Menu</span>
+        </button>
+        <Link href="/">
+          <a className="border-none">
+            <LogoHoriz className={`h-10 lg:hidden ml-4 ${dark ? 'text-white' : 'text-primary'}`}/>
+          </a>
+        </Link>
+      </div>
+      <Transition
+        show={isOpen}
+        enter="transition-all transform duration-75"
+        enterFrom="-translate-y-16 opacity-0"
+        enterTo="translate-y-0 opacity-100"
+        leave="transition-all transform duration-100"
+        leaveFrom="translate-y-0 opacity-100"
+        leaveTo="-translate-y-16 opacity-0"
+      >
+        <nav id="navigation" className="my-4">
+          <ul className="nav sm:flex sm:flex-col mb-16">
+            <MenuItems dark={dark}/>
+          </ul>
+        </nav>
+      </Transition>
     </div>
   )
 }
