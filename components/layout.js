6df08c8e5cac7ce0a04f63ab9e93a03ff2bf2703
aidanweltner <aidan.weltner@gmail.com>
2021-03-03 18:42:32 -0700

    added analytics tag

--- components/layout.js
+++ components/layout.js
@@ -16,6 +16,17 @@ export default function Layout({ children, home, project }) {
         <meta property="author" content="Todd Weltner"/>
         <meta property="og:site_name" content={siteTitle} />
         <meta property="og:locale" content="en_US" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129292435-4"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-129292435-4');
+            `,
+          }}
+        />
       </Head>
       
       <main>
