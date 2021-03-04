7497e3194009c1f10bde95d1d41eeaf7a56ed0be
aidanweltner <aidan.weltner@gmail.com>
2021-03-04 12:42:53 -0700

    updated footer hours

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ export default function Footer({dark}) {
     <>
       <RedFilter/>
       <footer className={`hidden lg:block lg:py-0 lg:px-0 lg:bg-opacity-0 lg:fixed lg:top-32 lg:right-8 xl:right-16 ${dark ? 'text-white' : 'text-primary'}`}>
-        <p>Hours: M-F 9AM-5PM</p>
+        <p>Hours: M-F 8AM-5PM</p>
         <p>Contact: <a className="border-b border-primary mb-3" href="tel:2083369860">208-336-9860</a></p>
         <p className="mt-3">Visit Us:</p>
         <a href="https://goo.gl/maps/BrcYDRhWNtEek6cy7" target="_blank">
