8b326f9c0c85b06dc262dabef252ebaceba3937f
aidanweltner <aidan.weltner@gmail.com>
2021-03-08 11:38:50 -0700

    added security tags for links

--- components/footer.js
+++ components/footer.js
@@ -8,7 +8,7 @@ export default function Footer({dark}) {
         <p>Hours: M-F 8AM-5PM</p>
         <p>Contact: <a className="border-b border-primary mb-3" href="tel:2083369860">208-336-9860</a></p>
         <p className="mt-3">Visit Us:</p>
-        <a href="https://goo.gl/maps/BrcYDRhWNtEek6cy7" target="_blank">
+        <a href="https://goo.gl/maps/BrcYDRhWNtEek6cy7" target="_blank" rel="noreferrer">
           <p>300 E 35th Street</p>
           <p>Garden City, ID</p>
           <p>83714</p>
